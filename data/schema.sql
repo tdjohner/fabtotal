659fc4fff323942aeea234d590840b5d2f48d805
Teagan <tdjohner@gmail.com>
2022-08-14 21:31:26 -0700

    Database compiles w/ ratings values

--- data/schema.sql
+++ data/schema.sql
@@ -9,8 +9,8 @@ Teagan Johner 2022-08-02
 CREATE TABLE account
 (
 	id INT IDENTITY(1,1) PRIMARY KEY,
-	username VARCHAR(64),
-	email VARCHAR(64),
+	username VARCHAR(128) UNIQUE,
+	email VARCHAR(128) UNIQUE,
 	lastLogin DATETIME,
 	emailValidated BIT,
 	createdOn DATETIME,
@@ -19,7 +19,7 @@ CREATE TABLE account
 CREATE TABLE post
 (
 	id INT IDENTITY(1,1) PRIMARY KEY,
-	posterAccountId INT NOT NULL, FOREIGN KEY (posterAccountId) REFERENCES account(id),
+	createdBy INT NOT NULL, FOREIGN KEY (createdBy) REFERENCES account(id),
 	price DECIMAL(17,2),
 	createdOn DATETIME,
 	lastModifiedOn DATETIME
@@ -29,5 +29,26 @@ CREATE TABLE postDescription
 (
 	id INT IDENTITY(1,1) PRIMARY KEY,
 	postId INT NOT NULL, FOREIGN KEY (postId) REFERENCES post(id),
-	accountId INT NOT NULL, FOREIGN KEY (accountId) REFERENCES account(id)
+	accountId INT NOT NULL, FOREIGN KEY (accountId) REFERENCES account(id),
+	content VARCHAR(MAX)
 );
+
+CREATE TABLE rating
+(
+	id INT IDENTITY(0,1) PRIMARY KEY
+);
+
+--Populate the rating table
+WHILE ((SELECT COUNT(*) FROM rating) < 6) BEGIN
+	INSERT dbo.rating DEFAULT VALUES
+END
+
+CREATE TABLE printerRating
+(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	createdBy INT NOT NULL, FOREIGN KEY (createdBy) REFERENCES account(id),
+	printerId INT NOT NULL, FOREIGN KEY (printerId) REFERENCES account(id),
+	rating INT NOT NULL, FOREIGN KEY (rating) REFERENCES rating(id)
+);
+
+--Want to store hashes in filepaths under folders named as hashes of account names
